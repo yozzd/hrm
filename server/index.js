cdd7dc2ff00b65a3dc661a8a099871988dc8e80b
yozz <yozzdharma@gmail.com>
2018-12-13 15:59:25 +0700

    refactor: server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,4 @@
-
 const express = require('express')
-const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
 const host = process.env.HOST || '0.0.0.0'
@@ -11,7 +9,7 @@ app.set('port', port)
 let config = require('../nuxt.config.js')
 config.dev = !(process.env.NODE_ENV === 'production')
 
-async function start() {
+const start = async () => {
   const nuxt = new Nuxt(config)
 
   if (config.dev) {
@@ -21,10 +19,7 @@ async function start() {
 
   app.use(nuxt.render)
 
-  app.listen(port, host)
-  consola.ready({
-    message: `Server listening on http://${host}:${port}`,
-    badge: true
-  })
+  await app.listen(port, host)
+  console.log(`Server listening on http://${host}:${port}`)
 }
 start()
