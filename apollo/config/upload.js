9d4e5b49a98e603e607ad27c5b6ba1ae9021b364
yozz <yozzdharma@gmail.com>
2019-01-28 16:16:35 +0700

    lint apollo config upload

--- apollo/config/upload.js
+++ apollo/config/upload.js
@@ -1,25 +1,25 @@
-import { createUploadLink } from 'apollo-upload-client'
-import { setContext } from 'apollo-link-context'
-import { InMemoryCache } from 'apollo-cache-inmemory'
+import { createUploadLink } from 'apollo-upload-client';
+import { setContext } from 'apollo-link-context';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 export default (ctx) => {
-  const uploadLink = createUploadLink({ uri: '/graphql' })
+  const uploadLink = createUploadLink({ uri: '/graphql' });
 
   const authLink = setContext((_, { headers }) => {
-    const token = ctx.app.$auth.token
+    const { token } = ctx.app.$auth;
     return {
       headers: {
         ...headers,
-        authorization: token ? `Bearer ${token}` : ''
-      }
-    }
-  })
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    };
+  });
 
-  const link = authLink.concat(uploadLink)
+  const link = authLink.concat(uploadLink);
 
   return {
     link,
     cache: new InMemoryCache(),
-    defaultHttpLink: false
-  }
-}
+    defaultHttpLink: false,
+  };
+};
