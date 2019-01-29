c40ac53bb193ca6d3ea29258c91e989a1a60c225
yozz <yozzdharma@gmail.com>
2019-01-29 16:36:40 +0700

    lint auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,8 @@
-import routeOption from '@/auth/auth.utilities';
-import userRoles from '@/auth/auth.roles';
+// prettier-ignore
+import routeOption from '../auth/auth.utilities';
+import userRoles from '../auth/auth.roles';
 
-export default function(ctx) {
+export default function (ctx) {
   if (routeOption(ctx.route, 'auth', false)) {
     return;
   }
@@ -11,13 +12,11 @@ export default function(ctx) {
   if (ctx.app.$auth.state.loggedIn) {
     if (login && ctx.route.path === login.split('?')[0]) {
       ctx.app.$auth.redirect('home');
-    } else {
-      if (
-        userRoles.indexOf(ctx.route.meta[0].guard) >
-        userRoles.indexOf(ctx.store.state.auth.user.role)
-      ) {
-        ctx.app.$auth.redirect('guard');
-      }
+    } else if (
+      userRoles.indexOf(ctx.route.meta[0].guard)
+      > userRoles.indexOf(ctx.store.state.auth.user.role)
+    ) {
+      ctx.app.$auth.redirect('guard');
     }
   } else {
     ctx.app.$auth.redirect('home');
